a48e596d122b20d0a54867a531a47f9d31110fd9
Bret Bernhoft <73839896+devbret@users.noreply.github.com>
2020-11-12 17:14:10 -0800

    Added quality checks for user input

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function main() {
-    //Main program variables.
+    //Primary program variables.
     const userInput = document.querySelector(`#userInput`);
     const submit = document.querySelector(`#submit`);
     const output = document.querySelector(`#output`);
@@ -8,14 +8,35 @@ function main() {
         //Making sure the user has added text to the input field, before attempting to process any data.
         if (userInput.value.length > 0) {
             //Capture and transform the user's input into an array of strings.
-            const data = userInput.value.split(` `);
+            const predata = userInput.value.split(` `);
+            //Creating the data array variable to hold all qualified words.
+            const data = [];
+            //Filtering all of the words to make sure they're strings with English characters in them.
+            for (let i = 0; i < predata.length; i++) {
+                //If the word is an empty string, ignore it.
+                if (predata[i] !== ``) {
+                    //Preparing each word that makes it through the first filter, by splitting it into an array of characters.
+                    const inner = predata[i].split(``);
+                    let holder = [];
+                    //Looping through and filtering out all non-english letters, including numbers and any punctuation.
+                    for (let z = 0; z < inner.length; z++) {
+                        const innerLetter = inner[z].toLowerCase();
+                        if (innerLetter.charCodeAt(0) >= 97 && innerLetter.charCodeAt(0) <= 122) {
+                            holder.push(innerLetter);
+                        }
+                    }
+                    //Joining the final result for each word; then pushing it to the data array as a qualified word for measurement.
+                    const finalAnswer = holder.join(``);
+                    data.push(finalAnswer);
+                }
+            }
             //Counting the number of letters there are in the user's input.
             const totalCharacters = data.reduce(function(t,d){
                 t += d.length;
                 return t;
             }, 0);
             //Displaying the results, which is the total number of characters divided by the total number of words submitted.
-            output.innerHTML = `The average word is ${(totalCharacters / data.length).toFixed(2)} characters long.`;
+            output.innerHTML = `The average word is <b>${(totalCharacters / data.length).toFixed(2)}</b> characters long.`;
         }
     });
 }
